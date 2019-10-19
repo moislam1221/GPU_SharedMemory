ea20aba7366fcb4617de28ca386d0c0106b53080
Mohammad <moislam@mit.edu>
2019-10-19 15:34:19 -0400

    Made python plots cleaner (e.g. used for loops in making plots rather than repeating code whenever possible). Added in capability to plot the results for different GPUs (useful for postprocessing data from machine with different GPUs - e.g. endeavour with GTX and TITAN)

--- 1D/PYTHON/postprocessing.py
+++ 1D/PYTHON/postprocessing.py
@@ -9,8 +9,10 @@ import pylab
 
 # LOAD DATA FROM FILES
 cpuData = pylab.loadtxt(open("../RESULTS/CPU_N1024_TOL1.txt"), delimiter=' ', usecols=(0,1,2,3))
-gpuData = pylab.loadtxt(open("../RESULTS/GPU_N1024_TOL1.txt"), delimiter=' ', usecols=(0,1,2,3,4))
-sharedData = pylab.loadtxt(open("../RESULTS/SHARED_N1024_TOL1.txt"), delimiter=' ', usecols=(0,1,2,3,4))
+gpuData_TITAN = pylab.loadtxt(open("../RESULTS/GPU_N1024_TOL1_TITAN_V.txt"), delimiter=' ', usecols=(0,1,2,3))
+sharedData_TITAN = pylab.loadtxt(open("../RESULTS/SHARED_N1024_TOL1_TITAN_V.txt"), delimiter=' ', usecols=(0,1,2,3,4))
+gpuData_GTX = pylab.loadtxt(open("../RESULTS/GPU_N1024_TOL1_GTX_1080Ti.txt"), delimiter=' ', usecols=(0,1,2,3))
+sharedData_GTX = pylab.loadtxt(open("../RESULTS/SHARED_N1024_TOL1_GTX_1080Ti.txt"), delimiter=' ', usecols=(0,1,2,3,4))
 
 # POSTPROCESS CPU DATA
 N = cpuData[0,0]
@@ -19,25 +21,34 @@ numTrials = len(cpuData[:,2])
 
 # POSTPROCESS GPU DATA
 tpb = [32, 64, 128, 256, 512]
-gpuTime = np.zeros(len(tpb))
+gpuTime_TITAN = np.zeros(len(tpb))
+gpuTime_GTX = np.zeros(len(tpb))
 for i in range(len(tpb)):
-    gpuTime[i] = np.mean(gpuData[i*numTrials:(i+1)*numTrials-1,3])
+    gpuTime_TITAN[i] = np.mean(gpuData_TITAN[i*numTrials:(i+1)*numTrials-1,3])
+    gpuTime_GTX[i] = np.mean(gpuData_TITAN[i*numTrials:(i+1)*numTrials-1,3])
 
-# POSTPROCESS GPU DATA
-sharedTime = np.zeros(len(tpb))
+# POSTPROCESS SHARED DATA
+sharedTime_TITAN = np.zeros(len(tpb))
+sharedTime_GTX = np.zeros(len(tpb))
 for i in range(len(tpb)):
-    sharedTime[i] = np.mean(sharedData[i*numTrials:(i+1)*numTrials-1,3])
+    sharedTime_TITAN[i] = np.mean(sharedData_TITAN[i*numTrials:(i+1)*numTrials-1,3])
+    sharedTime_GTX[i] = np.mean(sharedData_GTX[i*numTrials:(i+1)*numTrials-1,3])
 
 # CPU -> GPU
-cpuTogpu = cpuTime / np.array(gpuTime)
-gpuToshared = np.array(gpuTime) / np.array(sharedTime)
-cpuToshared = np.array(cpuTime) / np.array(sharedTime)
+cpuTogpu_TITAN = cpuTime / np.array(gpuTime_TITAN)
+gpuToshared_TITAN = np.array(gpuTime_TITAN) / np.array(sharedTime_TITAN)
+cpuToshared_TITAN = np.array(cpuTime_TITAN) / np.array(sharedTime_TITAN)
+cpuTogpu_GTX = cpuTime / np.array(gpuTime_GTX)
+gpuToshared_GTX = np.array(gpuTime_GTX) / np.array(sharedTime_GTX)
+cpuToshared_GTX = np.array(cpuTime_GTX) / np.array(sharedTime_GTX)
 
 # COMPARISON OF CPU-GPU-SHARED
 pylab.figure()
 pylab.semilogy(tpb, np.ones(len(tpb))*cpuTime, '--', linewidth=2,label = 'CPU')
-pylab.semilogy(tpb, gpuTime, '-o', linewidth=2, label = 'GPU')
-pylab.semilogy(tpb, sharedTime, '-o', linewidth=2, label = 'Shared')
+pylab.semilogy(tpb, gpuTime_TITAN, '-o', linewidth=2, label = 'TITAN V (Global Memory)')
+pylab.semilogy(tpb, sharedTime_TITAN, '-o', linewidth=2, label = 'TITAN V (Shared Memory)')
+pylab.semilogy(tpb, gpuTime_GTX, '-o', linewidth=2, label = 'GTX (Global Memory)')
+pylab.semilogy(tpb, sharedTime_GTX, '-o', linewidth=2, label = 'GTX (Shared Memory)')
 pylab.xlabel(r'Threads Per Block', fontsize = 20)
 pylab.ylabel(r'Time To Achieve TOL [ms]', fontsize = 20)
 pylab.xticks(fontsize = 16)
@@ -49,9 +60,12 @@ pylab.savefig('../FIGURES/cpu_gpu_shared.png')
 
 # SPEEDUP BETWEEN CPU-GPU-SHARED
 pylab.figure()
-pylab.plot(tpb, cpuTogpu, '-o', linewidth=2, label = 'CPU TO GPU')
-pylab.plot(tpb, gpuToshared, '-o', linewidth=2, label = 'GPU TO SHARED APPROACH')
-pylab.plot(tpb, cpuToshared, '-o', linewidth=2, label = 'GPU TO SHARED APPROACH')
+pylab.plot(tpb, cpuTogpu_TITAN, '-o', linewidth=2, label = 'TITAN: CPU TO GPU')
+pylab.plot(tpb, gpuToshared_TITAN, '-o', linewidth=2, label = 'TITAN: GPU TO SHARED APPROACH')
+pylab.plot(tpb, cpuToshared_TITAN, '-o', linewidth=2, label = 'TITAN: CPU TO SHARED APPROACH')
+pylab.plot(tpb, cpuTogpu_GTX, '-o', linewidth=2, label = 'GTX: CPU TO GPU')
+pylab.plot(tpb, gpuToshared_GTX, '-o', linewidth=2, label = 'GTX: GPU TO SHARED APPROACH')
+pylab.plot(tpb, cpuToshared_GTX, '-o', linewidth=2, label = 'GTX: CPU TO SHARED APPROACH')
 pylab.xlabel(r'Threads Per Block', fontsize = 20)
 pylab.ylabel(r'Speedup Achieved', fontsize = 20)
 pylab.xticks(fontsize = 16)
--- 1D/PYTHON/subiteration_plot.py
+++ 1D/PYTHON/subiteration_plot.py
@@ -8,26 +8,52 @@ import numpy as np
 import pylab
 
 # LOAD DATA FROM FILES
-tpb32Data = pylab.loadtxt(open("../SUBITERATIONS_RESULTS/N1024_TOL1_TPB32.txt"), delimiter=' ', usecols=(0,1,2,3))
-#tpb64Data = pylab.loadtxt(open("../OVERLAP_RESULTS/N1024_TOL1_TPB64.txt"), delimiter=' ', usecols=(0,1,2,3))
-#tpb128Data = pylab.loadtxt(open("../OVERLAP_RESULTS/N1024_TOL1_TPB128.txt"), delimiter=' ', usecols=(0,1,2,3))
-#tpb256Data = pylab.loadtxt(open("../OVERLAP_RESULTS/N1024_TOL1_TPB256.txt"), delimiter=' ', usecols=(0,1,2,3))
-#tpb512Data = pylab.loadtxt(open("../OVERLAP_RESULTS/N1024_TOL1_TPB512.txt"), delimiter=' ', usecols=(0,1,2,3))
 
-tpb = [32, 64, 128, 256, 512]
+GPU = ["TITAN_V", "GTX_1080Ti"]
+tpb = [32, 64, 128, 256] # 512]
 
 # TIME VS OVERLAP FOR VARIOUS TPB (= 32, 64, 128, 256, 1024)
-pylab.figure()
-numOverlap = range(0,tpb[0],2);
-numSubIterations = np.log(tpb[0]) / np.log(2)
-for i in range(0, numSubIterations):
-	index = range(0,numOverlap) + i * numOverlap
-	pylab.plot(range(0,tpb[0],2), tpb32Data[index,2], linewidth=2, label = 'hello')
-pylab.xlabel(r'Overlap', fontsize = 20)
-pylab.ylabel(r'Time To Achieve TOL [ms]', fontsize = 20)
-pylab.xticks(fontsize = 16)
-pylab.yticks(fontsize = 16)
-pylab.legend()
-pylab.grid()
-pylab.tight_layout()
-pylab.savefig('../FIGURES/subiterations_time.png')
+for g in range(0, len(gpu)):
+	for t in range(0, len(tpb)):
+		numOverlap = range(0,tpb[0],2);
+		numSubIterations = np.log(tpb[0]) / np.log(2)
+		pylab.figure()
+        dataPath = "../SUBITERATIONS_RESULTS/" + GPU[g] + "/N1024_TOL1_TPB" + str(tpb[t]) + ".txt"
+		data = pylab.loadtxt(open(dataPath), delimiter=' ', usecols=(0,1,2,3))
+		for i in range(0, numSubIterations):
+			index = range(0,numOverlap) + i * numOverlap
+			pylab.plot(range(0,tpb[t],2), data[index,2], linewidth=2, label = 'k = ' + str(data[index[0],1]))
+		pylab.xlabel(r'Overlap', fontsize = 20)
+		pylab.ylabel(r'Time To Achieve TOL [ms]', fontsize = 20)
+		pylab.xticks(fontsize = 16)
+		pylab.yticks(fontsize = 16)
+		pylab.legend()
+		pylab.grid()
+		pylab.tight_layout()
+        figurePath = "../FIGURES/subiterations_time_tpb" + str(tpb[t]) + "_" + GPU[g] + ".png")
+		pylab.savefig(figurePath)
+        
+
+# BEST TIME VS SUBITERATION FOR VARIOUS TPB (= 32, 64, 128, 256)
+for g in range(0, len(gpu)):
+	for t in range(0, len(tpb)):
+		numOverlap = range(0,tpb[0],2);
+		numSubIterations = np.log(tpb[0]) / np.log(2)
+        best_times = np.zeros(numSubIterations, 1)
+        subiterations_values = np.zeros(numSubIterations, 1)
+        dataPath = "../SUBITERATIONS_RESULTS/" + GPU[g] + "/N1024_TOL1_TPB" + str(tpb[t]) + ".txt"
+		data = pylab.loadtxt(open(dataPath), delimiter=' ', usecols=(0,1,2,3))
+		for i in range(0, numSubIterations):
+			index = range(0,numOverlap) + i * numOverlap
+            best_times[i] = np.min(data[index, 2])
+            subiteration_values[i] = data[index[0], 1]
+		pylab.plot(subiteration_values, best_times, linewidth=2, label = 'tpb = ' + tpb[t])
+    pylab.xlabel(r'Subiterations', fontsize = 20)
+	pylab.ylabel(r'Time To Achieve TOL [ms]', fontsize = 20)
+	pylab.xticks(fontsize = 16)
+	pylab.yticks(fontsize = 16)
+	pylab.legend()
+	pylab.grid()
+	pylab.tight_layout()
+	figurePath = "../FIGURES/subiterations_time_best" + "_" + GPU[g] + ".png")
+	pylab.savefig(figurePath)
