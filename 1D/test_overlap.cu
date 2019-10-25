b6cd80693eb1007fdf4f227be4090849f140fa13
Mohammad <moislam@mit.edu>
2019-10-25 13:14:46 -0400

    Updated test overlap and test subiterations scripts to incorporate longer subdomains, now analyze time required to reduce tolerance by some factor, rather than achieve specific tolerance

--- 1D/test_overlap.cu
+++ 1D/test_overlap.cu
@@ -21,6 +21,7 @@
 #include "Helper/residual.h"
 #include "Helper/setGPU.h"
 #include "jacobi-1D-shared.h"
+#include "jacobi-1D-longer-subdomain-shared.h"
 
 // IDEA: For N = 1024, create a plot of convergence time as the overlap points increase
 
@@ -34,12 +35,14 @@ int main(int argc, char *argv[])
     setGPU(gpuToUse);
 
     // INPUTS AND OUTPUT FILE NAMES
-    const int nDim = 1024; //atoi(argv[1]); 
-    const int threadsPerBlock = 32; //atoi(argv[2]); 
-    const float TOL = 1.0; //atoi(argv[4]);
-    const int trials = 20; // 20
-    const int subIterations = threadsPerBlock/2; //atoi(argv[2]); 
-    std::string FILENAME = "OVERLAP_RESULTS/N1024_TOL1_TPB32.txt";
+    const int nDim = 65536; //atoi(argv[1]);
+    const int threadsPerBlock = 512; //atoi(argv[2]);
+    // const float TOL = 1.0; //atoi(argv[4]);
+    const int innerSubdomainLength = 1024; 
+    const float residualReductionFactor = 10000.0;
+    const int trials = 1; // 20
+    const int subIterations = threadsPerBlock/2; //atoi(argv[2]);
+    std::string FILENAME = "OVERLAP_RESULTS/TITAN_V/N65536_TOLREDUCE10000_TPB512.txt";
     /////////////////////////////////////////////////////////////////////////
  
     // INITIALIZE ARRAYS
@@ -58,8 +61,12 @@ int main(int argc, char *argv[])
         rhs[iGrid] = 1.0f;
     }
 
+    // COMPUTE INITIAL RESIDUAL AND SET TOLERANCE
+    float initResidual = residual1DPoisson(initX, rhs, nGrids);
+    const float TOL = initResidual / residualReductionFactor; //atoi(argv[4]);
+
     // NUMBER OF OVERLAP PARAMETERS TO EXPLORE
-    int numIterations = (threadsPerBlock-2)/2;
+    int numIterations = (innerSubdomainLength-2)/2;
     
     // NECESSARY CONTAINERS
     int * sharedCycles = new int[subIterations];
@@ -78,22 +85,39 @@ int main(int argc, char *argv[])
     for (int i = 0; i <= numIterations; i++) {
         // OBTAIN NUMBER OF CYCLES TO CONVERGE FOR GIVEN OVERLAP
         OVERLAP = 2*i;
-        sharedCycles[i] = jacobiSharedIterationCount(initX, rhs, nGrids, TOL, threadsPerBlock, OVERLAP, subIterations);
+        if (threadsPerBlock == innerSubdomainLength) {
+			sharedCycles[i] = jacobiSharedIterationCount(initX, rhs, nGrids, TOL, threadsPerBlock, OVERLAP, subIterations);
+        }
+        else {
+			sharedCycles[i] = jacobiSharedLongerSubdomainIterationCount(initX, rhs, nGrids, TOL, threadsPerBlock, OVERLAP, subIterations, innerSubdomainLength);
+        }
         for (int iter = 0; iter < trials; iter++) {
-            // GET FINAL SOLUTION
-			cudaEventRecord(start_sh, 0);
-			solutionJacobiShared = jacobiShared(initX, rhs, nGrids, sharedCycles[i], threadsPerBlock, OVERLAP, subIterations);
-			// OBTAIN FINAL TIMES REQUIRED
-			cudaEventRecord(stop_sh, 0);
-			cudaEventSynchronize(stop_sh);
-			cudaEventElapsedTime(&sharedJacobiTime, start_sh, stop_sh);
-			totalTime = totalTime + sharedJacobiTime;
+            if (threadsPerBlock == innerSubdomainLength) {
+				// GET FINAL SOLUTION
+				cudaEventRecord(start_sh, 0);
+				solutionJacobiShared = jacobiShared(initX, rhs, nGrids, sharedCycles[i], threadsPerBlock, OVERLAP, subIterations);
+				// OBTAIN FINAL TIMES REQUIRED
+				cudaEventRecord(stop_sh, 0);
+				cudaEventSynchronize(stop_sh);
+				cudaEventElapsedTime(&sharedJacobiTime, start_sh, stop_sh);
+				totalTime = totalTime + sharedJacobiTime;
+            }
+            else {
+				// GET FINAL SOLUTION
+				cudaEventRecord(start_sh, 0);
+				solutionJacobiShared = jacobiSharedLongerSubdomain(initX, rhs, nGrids, sharedCycles[i], threadsPerBlock, OVERLAP, subIterations, innerSubdomainLength);
+				// OBTAIN FINAL TIMES REQUIRED
+				cudaEventRecord(stop_sh, 0);
+				cudaEventSynchronize(stop_sh);
+				cudaEventElapsedTime(&sharedJacobiTime, start_sh, stop_sh);
+				totalTime = totalTime + sharedJacobiTime;
+            }
             printf("FINISHED ITERATION %d\n", iter);
         }
         sharedJacobiTimeArray[i] = totalTime / trials;
         residualJacobiShared[i] = residual1DPoisson(solutionJacobiShared, rhs, nGrids);
         printf("Residual is %f\n", residualJacobiShared[i]);
-        printf("FINISHED OVERLAP = %d/%d CASE (N = %d, tpb = %d)\n", OVERLAP, threadsPerBlock-2, nDim, threadsPerBlock);
+        printf("FINISHED OVERLAP = %d/%d CASE (N = %d, tpb = %d)\n", OVERLAP, innerSubdomainLength-2, nDim, threadsPerBlock);
         totalTime = 0.0;
     }    
 
@@ -103,6 +127,7 @@ int main(int argc, char *argv[])
     printf("GPU Name: %s\n", gpuToUse.c_str());
     printf("Number of unknowns: %d\n", nDim);
     printf("Threads Per Block: %d\n", threadsPerBlock);
+    printf("Number of Subdomain Points handled by each block: %d\n", innerSubdomainLength);
     printf("======================================================\n");
    
     std::ofstream timings_sh;
--- 1D/test_subiterations.cu
+++ 1D/test_subiterations.cu
@@ -21,6 +21,7 @@
 #include "Helper/residual.h"
 #include "Helper/setGPU.h"
 #include "jacobi-1D-shared.h"
+#include "jacobi-1D-longer-subdomain-shared.h"
 
 // IDEA: For N = 1024, create a plot of convergence time as the overlap points increase
 
@@ -32,13 +33,16 @@ int main(int argc, char *argv[])
     // ENDEAVOUR GPUs: "TITAN V" OR "GeForce GTX 1080 Ti"
     std::string gpuToUse = "Quadro K420";
     setGPU(gpuToUse);
+    // INPUTS AND OUTPUT FILE NAMES
 
     // INPUTS AND OUTPUT FILE NAMES
-    const int nDim = 1024; //atoi(argv[1]); 
-    const int threadsPerBlock = 32; //atoi(argv[2]); 
-    const float TOL = 1.0; //atoi(argv[4]);
-    const int nIters = 20;
-    std::string FILENAME = "SUBITERATION_RESULTS/N1024_TOL1_TPB32.txt";
+    const int nDim = 65536; //atoi(argv[1]);
+    const int threadsPerBlock = 256; //atoi(argv[2]);
+    const int innerSubdomainLength = 256;
+    // const float TOL = 1.0; //atoi(argv[4]);
+    const float residualReductionFactor = 10000.0;
+    const int trials = 1;
+    std::string FILENAME = "SUBITERATION_RESULTS/TITAN_V/N65536_TOLREDUCE10000_TPB256.txt";
     /////////////////////////////////////////////////////////////////////////
 
     // INITIALIZE ARRAYS
@@ -57,10 +61,14 @@ int main(int argc, char *argv[])
         rhs[iGrid] = 1.0f;
     }
 
+    // COMPUTE INITIAL RESIDUAL AND SET TOLERANCE
+    float initResidual = residual1DPoisson(initX, rhs, nGrids);
+    const float TOL = initResidual / residualReductionFactor; //atoi(argv[4]);
+
     // NUMBER OF SUBITERATION AND OVERLAP PARAMETERS TO EXPLORE
-    int numOverlap = threadsPerBlock/2;
+    int numOverlap = innerSubdomainLength/2;
     int numSubIteration = 0;
-    for (int i = threadsPerBlock / 4; i < threadsPerBlock * threadsPerBlock; i = i * 2) {
+    for (int i = innerSubdomainLength / 4; i < innerSubdomainLength * innerSubdomainLength; i = i * 2) {
         numSubIteration = numSubIteration + 1;
     }
     int numIterations = numOverlap * numSubIteration;
@@ -82,8 +90,9 @@ int main(int argc, char *argv[])
     printf("GPU Name: %s\n", gpuToUse.c_str());
     printf("Number of unknowns: %d\n", nDim);
     printf("Threads Per Block: %d\n", threadsPerBlock);
-    printf("SubIteration Values to Explore: [%d %d]\n", threadsPerBlock/4, threadsPerBlock * threadsPerBlock/2);
-    printf("Overlap Range to Explore: [%d, %d]\n", 0, threadsPerBlock-2);
+    printf("Number of Points handled by each block: %d\n", innerSubdomainLength);
+    printf("SubIteration Values to Explore: [%d %d]\n", innerSubdomainLength/4, innerSubdomainLength * innerSubdomainLength/2);
+    printf("Overlap Range to Explore: [%d, %d]\n", 0, innerSubdomainLength-2);
     printf("======================================================\n");
     
     int OVERLAP, SUBITERATIONS;
@@ -92,29 +101,46 @@ int main(int argc, char *argv[])
     float totalTime = 0.0;
     // VARY NUMBER OF SUBITERATIONS
     for (int k = 0; k < numSubIteration; k++) {
-        SUBITERATIONS = (threadsPerBlock / 4) * pow(2, k);
+        SUBITERATIONS = (innerSubdomainLength / 4) * pow(2, k);
 		// VARY OVERLAP
         for (int i = 0; i < numOverlap; i++) {
 			// OBTAIN NUMBER OF CYCLES TO CONVERGE FOR GIVEN COMBINATION OF OVERLAP AND SUBITERATIONS
 			OVERLAP = 2*i;
             index = i + k * numOverlap;
-			sharedCycles[index] = jacobiSharedIterationCount(initX, rhs, nGrids, TOL, threadsPerBlock, OVERLAP, SUBITERATIONS);
-            // PERFORM 20 TRIALS
-			for (int iter = 0; iter < nIters; iter++) {
-				// GET FINAL SOLUTION
-				cudaEventRecord(start_sh, 0);
-				solutionJacobiShared = jacobiShared(initX, rhs, nGrids, sharedCycles[index], threadsPerBlock, OVERLAP, SUBITERATIONS);
-				// OBTAIN FINAL TIMES REQUIRED
-				cudaEventRecord(stop_sh, 0);
-				cudaEventSynchronize(stop_sh);
-				cudaEventElapsedTime(&sharedJacobiTime, start_sh, stop_sh);
-				totalTime = totalTime + sharedJacobiTime;
+            if (threadsPerBlock == innerSubdomainLength) {
+				sharedCycles[index] = jacobiSharedIterationCount(initX, rhs, nGrids, TOL, threadsPerBlock, OVERLAP, SUBITERATIONS);
+            }
+            else {
+				sharedCycles[index] = jacobiSharedLongerSubdomainIterationCount(initX, rhs, nGrids, TOL, threadsPerBlock, OVERLAP, SUBITERATIONS, innerSubdomainLength);
+            }
+            // PERFORM TRIALS
+			for (int iter = 0; iter < trials; iter++) {
+				if (threadsPerBlock == innerSubdomainLength) {
+					// GET FINAL SOLUTION
+					cudaEventRecord(start_sh, 0);
+					solutionJacobiShared = jacobiShared(initX, rhs, nGrids, sharedCycles[index], threadsPerBlock, OVERLAP, SUBITERATIONS);
+					// OBTAIN FINAL TIMES REQUIRED
+					cudaEventRecord(stop_sh, 0);
+					cudaEventSynchronize(stop_sh);
+					cudaEventElapsedTime(&sharedJacobiTime, start_sh, stop_sh);
+					totalTime = totalTime + sharedJacobiTime;
+                }
+				else {
+					// GET FINAL SOLUTION
+					cudaEventRecord(start_sh, 0);
+					solutionJacobiShared = jacobiSharedLongerSubdomain(initX, rhs, nGrids, sharedCycles[index], threadsPerBlock, OVERLAP, SUBITERATIONS, innerSubdomainLength);
+					// OBTAIN FINAL TIMES REQUIRED
+					cudaEventRecord(stop_sh, 0);
+					cudaEventSynchronize(stop_sh);
+					cudaEventElapsedTime(&sharedJacobiTime, start_sh, stop_sh);
+					totalTime = totalTime + sharedJacobiTime;
+                }
 				printf("FINISHED ITERATION %d\n", iter);
 			}
-			sharedJacobiTimeArray[index] = totalTime / nIters;
+			sharedJacobiTimeArray[index] = totalTime / trials;
 			residualJacobiShared[index] = residual1DPoisson(solutionJacobiShared, rhs, nGrids);
             printf("Residual is %f\n", residualJacobiShared[index]);
-			printf("FINISHED SUBITERATIONS %d/%d OVERLAP = %d/%d CASE (N = %d, tpb = %d)\n", SUBITERATIONS, threadsPerBlock * threadsPerBlock / 2, OVERLAP, threadsPerBlock-2, nDim, threadsPerBlock);
+			printf("FINISHED SUBITERATIONS %d/%d OVERLAP = %d/%d CASE (N = %d, tpb = %d)\n", SUBITERATIONS, innerSubdomainLength * innerSubdomainLength / 2, OVERLAP, innerSubdomainLength-2, nDim, threadsPerBlock);
 			totalTime = 0.0;
 		}    
     }
