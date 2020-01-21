2ca8164fe9070aebb470159b7cec704a90a96299
Mohammad Shafaet Islam <mohammad@AIRFOIL.MIT.EDU>
2020-01-21 18:46:40 -0500

    Moved rhs array to shared memory

--- 1D/jacobi-1D-shared.h
+++ 1D/jacobi-1D-shared.h
@@ -1,8 +1,8 @@
 __device__
-void __jacobiUpdateKernel(const float * rhsBlock, const int nGrids, const int nSub, const int OVERLAP, const int subIterations)
+void __jacobiUpdateKernel(const int nGrids, const int nSub, const int OVERLAP, const int subIterations)
 {
     extern __shared__ float sharedMemory[];
-    float * x0 = sharedMemory, * x1 = sharedMemory + nSub;
+    float * x0 = sharedMemory, * x1 = sharedMemory + nSub, * x2 = sharedMemory + 2 * nSub;
 
     const float dx = 1.0 / (nGrids - 1);
 
@@ -12,7 +12,8 @@ void __jacobiUpdateKernel(const float * rhsBlock, const int nGrids, const int nS
         if (I < nGrids-1) {
             float leftX = x0[i-1];
             float rightX = x0[i+1];
-            x1[i] = jacobi1DPoisson(leftX, rightX, rhsBlock[i], dx);
+            float rhs = x2[i];
+            x1[i] = jacobi1DPoisson(leftX, rightX, rhs, dx);
         }
         __syncthreads();
         float * tmp = x1; x1 = x0; x0 = tmp;
@@ -34,6 +35,7 @@ void _jacobiUpdate(float * x1Gpu, const float * x0Gpu, const float * rhsGpu, con
     if (I < nGrids) {
         sharedMemory[i] = x0Gpu[I];
         sharedMemory[i + nPerSubdomain] = x0Gpu[I];
+        sharedMemory[i + 2 * nPerSubdomain] = rhsGpu[I]; // added
     }
 
     const int I2 = blockDim.x + (threadIdx.x + (blockDim.x - OVERLAP) * blockIdx.x);
@@ -41,15 +43,14 @@ void _jacobiUpdate(float * x1Gpu, const float * x0Gpu, const float * rhsGpu, con
     if (i2 < nPerSubdomain && I2 < nGrids) { 
         sharedMemory[i2] = x0Gpu[I2];
         sharedMemory[i2 + nPerSubdomain] = x0Gpu[I2];
+        sharedMemory[i2 + 2 * nPerSubdomain] = rhsGpu[I2]; // added
     }
-
-    const float * rhsBlock = rhsGpu + (blockDim.x - OVERLAP) * blockIdx.x;
 
     // Synchronize all threads before entering kernel
     __syncthreads();
 
     // STEP 2 - UPDATE ALL INNER POINTS IN EACH BLOCK
-    __jacobiUpdateKernel(rhsBlock, nGrids, nPerSubdomain, OVERLAP, subIterations);
+    __jacobiUpdateKernel(nGrids, nPerSubdomain, OVERLAP, subIterations);
 
     // STEP 3 - MOVE ALL VALUES FROM SHARED MEMORY BACK TO GLOBAL MEMORY
     // Move back to global memory (if overlap = 0 ONLY)
@@ -98,7 +99,7 @@ float * jacobiShared(const float * initX, const float * rhs, const int nGrids, c
     cudaMemcpy(rhsGpu, rhs, sizeof(float) * nGrids, cudaMemcpyHostToDevice);
 
     // Define amount of shared memory needed
-    const int sharedBytes = 2 * nSub * sizeof(float);
+    const int sharedBytes = 3 * nSub * sizeof(float);
 
     // Call kernel to allocate to sharedmemory and update points
     for (int step = 0; step < cycles; step++) {
@@ -139,7 +140,7 @@ int jacobiSharedIterationCount(const float * initX, const float * rhs, const int
     cudaMemcpy(rhsGpu, rhs, sizeof(float) * nGrids, cudaMemcpyHostToDevice);
 
     // Define amount of shared memory needed
-    const int sharedBytes = 2 * nSub * sizeof(float);
+    const int sharedBytes = 3 * nSub * sizeof(float);
 
     // Call kernel to allocate to sharedmemory and update points
     float residual = 1000000000000.0;
@@ -153,7 +154,7 @@ int jacobiSharedIterationCount(const float * initX, const float * rhs, const int
         nIters++;
         cudaMemcpy(solution, x0Gpu, sizeof(float) * nGrids, cudaMemcpyDeviceToHost);
         residual = residual1DPoisson(solution, rhs, nGrids);
-/*        if (nIters % 1000 == 0) {
+/*        if (nIters % 10 == 0) {
             printf("Shared: The residual is %f\n", residual);
         }
 */    }
