7eabc3c3cfd75297c56c36530f157e8c63e89e20
Mohammad Shafaet Islam <mohammad@AIRFOIL.MIT.EDU>
2020-01-27 20:12:50 -0500

    CPU/GPU implementations now use solution error as basis for convergence

--- 1D/jacobi-1D-cpu.h
+++ 1D/jacobi-1D-cpu.h
@@ -38,7 +38,7 @@ float * jacobiCpu(const float * initX, const float * rhs, int nGrids, int nIters
     return x0;
 }
 
-int jacobiCpuIterationCount(const float * initX, const float * rhs, int nGrids, float TOL)
+int jacobiCpuIterationCount(const float * initX, const float * solution_exact, const float * rhs, int nGrids, float TOL)
 {
     float dx = 1.0 / (nGrids - 1);
     float * x0 = new float[nGrids];
@@ -46,9 +46,11 @@ int jacobiCpuIterationCount(const float * initX, const float * rhs, int nGrids, 
     memcpy(x0, initX, sizeof(float) * nGrids);
     memcpy(x1, initX, sizeof(float) * nGrids);
 
-    float residual = 1000000000000.0;
+    // float residual = 1000000000000.0;
+    float solution_error = 1000000000000.0;
     int iIter = 0;
-    while (residual > TOL) {
+    // while (residual > TOL) {
+    while (solution_error > TOL) {
         for (int iGrid = 1; iGrid < nGrids-1; ++iGrid) {
             float leftX = x0[iGrid - 1];
             float rightX = x0[iGrid + 1];
@@ -56,11 +58,14 @@ int jacobiCpuIterationCount(const float * initX, const float * rhs, int nGrids, 
         }
         float * tmp = x0; x0 = x1; x1 = tmp;
         iIter++;
-        residual = residual1DPoisson(x0, rhs, nGrids);
+		// residual = residual1DPoisson(x0, rhs, nGrids);
+		solution_error = solutionError1DPoisson(x0, solution_exact, nGrids); 
         if (iIter % 1000 == 0) {
-            printf("CPU: The residual at step %d is %f\n", iIter, residual);
-        }
+			// printf("CPU: The residual at step %d is %f\n", iIter, residual);
+			printf("CPU: The solution error at step %d is %f\n", iIter, solution_error);
+		}
     }
+
     int nIters = iIter;
     delete[] x1;
     return nIters;
--- 1D/jacobi-1D-gpu.h
+++ 1D/jacobi-1D-gpu.h
@@ -67,43 +67,50 @@ float * jacobiGpu(const float * initX, const float * rhs, const int nGrids, cons
     return solution;
 }
 
-int jacobiGpuIterationCount(const float * initX, const float * rhs, const int nGrids, const float TOL,
-                                   const int threadsPerBlock)
+int jacobiGpuIterationCount(const float * initX, const float * solution_exact, const float * rhs, const int nGrids, const float TOL, const int threadsPerBlock)
 {
     // Compute dx for use in jacobi1DPoisson
+    float solution_error = 1000000000000.0;
     float dx = 1.0 / (nGrids - 1);
     
     // Allocate memory in the CPU for all inputs and solutions
-    float * x0Gpu, * x1Gpu, * rhsGpu, * residualGpu;
+    float * x0Gpu, * x1Gpu, * rhsGpu, * solutionErrorGpu, * solution_exactGpu; // * residualGpu;
     cudaMalloc(&x0Gpu, sizeof(float) * nGrids);
     cudaMalloc(&x1Gpu, sizeof(float) * nGrids);
     cudaMalloc(&rhsGpu, sizeof(float) * nGrids);
-    cudaMalloc(&residualGpu, sizeof(float) * nGrids);
+    // cudaMalloc(&residualGpu, sizeof(float) * nGrids);
+    cudaMalloc(&solutionErrorGpu, sizeof(float) * nGrids);
+    cudaMalloc(&solution_exactGpu, sizeof(float) * nGrids);
 
     // Allocate GPU memory
     cudaMemcpy(x0Gpu, initX, sizeof(float) * nGrids, cudaMemcpyHostToDevice);
     cudaMemcpy(x1Gpu, initX, sizeof(float) * nGrids, cudaMemcpyHostToDevice);
     cudaMemcpy(rhsGpu, rhs, sizeof(float) * nGrids, cudaMemcpyHostToDevice);
-    cudaMemcpy(residualGpu, rhs, sizeof(float) * nGrids, cudaMemcpyHostToDevice);
+    // cudaMemcpy(residualGpu, rhs, sizeof(float) * nGrids, cudaMemcpyHostToDevice);
+    cudaMemcpy(solutionErrorGpu, rhs, sizeof(float) * nGrids, cudaMemcpyHostToDevice);
+    cudaMemcpy(solution_exactGpu, solution_exact, sizeof(float) * nGrids, cudaMemcpyHostToDevice);
 
     // Run the classic iteration for prescribed number of iterations
     int nBlocks = (int)ceil(nGrids / (float)threadsPerBlock);
     float residual = 1000000000000.0;
     int iIter = 0;
     float * solution = new float[nGrids];
-    float * residualCpu = new float[nGrids];
-    while (residual > TOL) {
-	// Jacobi iteration on the GPU
+    // float * residualCpu = new float[nGrids];
+    float * solutionErrorCpu = new float[nGrids];
+    // while (residual > TOL) {
+	while (solution_error > TOL) {
+    // Jacobi iteration on the GPU
         _jacobiGpuClassicIteration<<<nBlocks, threadsPerBlock>>>(
                 x1Gpu, x0Gpu, rhsGpu, nGrids, dx); 
         {
             float * tmp = x0Gpu; x0Gpu = x1Gpu; x1Gpu = tmp;
         }
         iIter++;
+        // RESIDUAL CALCULATION
         // Write solution from GPU to CPU variable
-		// cudaMemcpy(solution, x0Gpu, sizeof(float) * nGrids, cudaMemcpyDeviceToHost);
-		// residual = residual1DPoisson(solution, rhs,  nGrids);
-        residual1DPoissonGPU <<<nBlocks, threadsPerBlock>>> (residualGpu, x0Gpu, rhsGpu, nGrids);
+//		cudaMemcpy(solution, x0Gpu, sizeof(float) * nGrids, cudaMemcpyDeviceToHost);
+//		residual = residual1DPoisson(solution, rhs,  nGrids);
+/*        residual1DPoissonGPU <<<nBlocks, threadsPerBlock>>> (residualGpu, x0Gpu, rhsGpu, nGrids);
         cudaDeviceSynchronize();
         cudaMemcpy(residualCpu, residualGpu, sizeof(float) * nGrids, cudaMemcpyDeviceToHost);
         residual = 0.0;
@@ -114,13 +121,35 @@ int jacobiGpuIterationCount(const float * initX, const float * rhs, const int nG
         if (iIter % 1000 == 0) {
 			printf("GPU: The residual at step %d is %f\n", iIter, residual);
         }
+*/
+
+//      ERROR CALCULATION
+//		cudaMemcpy(solution, x0Gpu, sizeof(float) * nGrids, cudaMemcpyDeviceToHost);
+//		solution_error = solutionError1DPoisson(solution, solution_exact, nGrids);
+        solutionError1DPoissonGPU <<<nBlocks, threadsPerBlock>>> (solutionErrorGpu, x0Gpu, solution_exactGpu, nGrids);
+        cudaDeviceSynchronize();
+        cudaMemcpy(solutionErrorCpu, solutionErrorGpu, sizeof(float) * nGrids, cudaMemcpyDeviceToHost);
+        solution_error = 0.0;
+        for (int j = 0; j < nGrids; j++) {
+             solution_error = solution_error + solutionErrorCpu[j];
+        }
+        solution_error = sqrt(solution_error);
+        if (iIter % 1000 == 0) {
+			printf("GPU: The solution error at step %d is %f\n", iIter, solution_error);
+        }
     }
+
     // Free all memory
+    // CPU
     delete[] solution;
-    delete[] residualCpu;
+    // delete[] solution_error;
+    // delete[] residualCpu;
+    delete[] solutionErrorCpu;
+    // GPU
     cudaFree(x0Gpu);
     cudaFree(x1Gpu);
     cudaFree(rhsGpu);
+    cudaFree(solutionErrorGpu);
 
     int nIters = iIter;
     return nIters;
